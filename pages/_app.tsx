240cf07e414e760ced3cd48bc49f879ae8fbb074
Desoindx <xavier.desoindre@hotmail.fr>
2023-04-18 10:46:50 +0200

    chore(analysis): Add hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,6 +49,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:description" content={content.description} />
         <link rel="icon" href="/favicon.svg" />
         <script
+          type="text/javascript"
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
             __html: `
@@ -65,6 +66,38 @@ export default function App({ Component, pageProps }: AppProps) {
                 g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
               })();
               `,
+          }}
+        />
+        <script
+          type="text/javascript"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+                /* Crisp */
+                window.$crisp=[];
+                window.CRISP_WEBSITE_ID="a773553c-07e5-4c2e-b0f7-f7946ce35435";
+                (function(){d=document;s=d.createElement("script");
+                s.src="https://client.crisp.chat/l.js";
+                s.async=1;
+                d.getElementsByTagName("head")[0].appendChild(s);
+                })();`,
+          }}
+        />
+        <script
+          type="text/javascript"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r) {
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3455550,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');
+              r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
+          `,
           }}
         />
       </Head>
